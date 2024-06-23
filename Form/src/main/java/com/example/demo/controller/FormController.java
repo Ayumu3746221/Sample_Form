0f370a358616132baee4db85b8933f04990e5933
Ayumu3746221 <a.kkt0307@gmail.com>
2024-06-23 09:17:52 +0900

    "HTML&CSS update"

--- Form/src/main/java/com/example/demo/controller/FormController.java
+++ Form/src/main/java/com/example/demo/controller/FormController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FormController {
 	@GetMapping("/")
-	public String index() {
+	public String Home() {
 		return "Home";
+	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "form";
 	}
 }
