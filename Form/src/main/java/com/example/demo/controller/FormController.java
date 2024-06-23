40cc8a352faf910b3dbc790410b7421a02754038
Ayumu3746221 <a.kkt0307@gmail.com>
2024-06-23 10:58:11 +0900

    confirm add

--- Form/src/main/java/com/example/demo/controller/FormController.java
+++ Form/src/main/java/com/example/demo/controller/FormController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.data.FormData;
+
+import org.springframework.ui.Model;
 
 @Controller
 public class FormController {
@@ -12,7 +18,15 @@ public class FormController {
 	}
 	
 	@GetMapping("/form")
-	public String form() {
+	public String form(Model model) {
+		model.addAttribute("formData",new FormData());
 		return "form";
+	}
+	
+	@PostMapping("/confirmation")
+	public ModelAndView toForm(@ModelAttribute FormData formData, ModelAndView mv) {
+		mv.setViewName("confirmation");
+		mv.addObject("formData", formData);
+		return mv;
 	}
 }
---
+++ Form/src/main/java/com/example/demo/data/FormData.java
@@ -0,0 +1,12 @@
+package com.example.demo.data;
+
+import lombok.Data;
+
+@Data
+public class FormData {
+	private String name;
+	private int age;
+	private String email;
+	private int gender;
+	private String content;
+}
